905c0c5ce6249b1e20357a5875e661bcafa2de37
Edumachadoh <dudu.henrique.em@gmail.com>
2025-05-06 21:51:17 -0300

    Ajustando Swagger

--- api/SistemaRestaurante/Program.cs
+++ api/SistemaRestaurante/Program.cs
@@ -3,19 +3,24 @@ using SistemaRestaurante.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var conectionString = builder.Configuration.GetConnectionString("AppDbConnectionString");
-builder.Services.AddDbContext<AppDbContext>(x => x.UseMySql(conectionString, ServerVersion.AutoDetect(conectionString)));
+var connectionString = builder.Configuration.GetConnectionString("AppDbConnectionString");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
